4b8419b865391c82c99e4f5f39c6a854f2e9c774
Harsh Khandelwal <harskhandelwal1805@gmail.com>
2023-08-14 00:44:27 +0530

    Create nfts.js

---
+++ Poly mod 1/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: '0',
+        description: 'Airbrush portrait of wolverine from xmen, by dimitri patelis marvel masterpieces , romero brito',
+        image: 'Qmd9nvWNzS1BLAZU7gftEH8uMu99bBgjnf2cRUz9VRFt7x'
+    },
+    {
+        name: '1',
+        description: 'Airbrush portrait of wolverine from xmen, by dimitri patelis marvel masterpieces , romero brito',
+        image: 'QmTZhBw1r34G8jpMVchMSZAQUSVHGxa46s1G4vyLuX4XeL'
+    },//change
+    {
+        name: '2',
+        description: 'Airbrush portrait of wolverine from xmen, by dimitri patelis marvel masterpieces , romero brito',
+        image: 'QmXTR8S8yRqeWmaotQ4TgzMxN6zH2Ddi3VSKPPFhpZnp2f'
+    },
+    {
+        name: '3',
+        description: 'Airbrush portrait of wolverine from xmen, by dimitri patelis marvel masterpieces , romero brito',
+        image: 'QmeXzQeP8LM7wuwnVvn6jh9XbjVofGJDdKcoj6Bc3gJG2L'
+    },
+    {
+        name: '4',
+        description: 'Airbrush portrait of wolverine from xmen, by dimitri patelis marvel masterpieces , romero brito',
+        image: 'QmdAfDGDvGmVqxtKJyWHAAnFYSfYu3V3XMPCPFr7NidATX'
+    },    
+];
+
+module.exports = nfts;
