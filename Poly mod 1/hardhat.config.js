4e797af7ee9e377d3e3060fe51013a1f71623716
Harsh Khandelwal <harskhandelwal1805@gmail.com>
2023-08-14 00:42:28 +0530

    Create hardhat.config.js

---
+++ Poly mod 1/hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.18",
+  networks: {
+    mumbai:{
+      url: `https://rpc.ankr.com/polygon_mumbai`,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+    goerli: {
+      url: 'https://ethereum-goerli.publicnode.com',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  }
+};
